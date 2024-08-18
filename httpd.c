8576976c390f465388dc0fcfe024296328753141
Jaehwang Kim <kim.jaehwang@gmail.com>
2024-08-18 17:20:12 +0900

    Add comment

--- httpd.c
+++ httpd.c
@@ -99,6 +99,16 @@ void start_listening(int server_fd) {
     }
 }
 
+/**
+ * @brief HTTP 서버의 메인 함수입니다.
+ *
+ * 이 함수는 HTTP 서버를 초기화하고 주요 이벤트 루프를 시작하는 역할을 담당합니다.
+ * 네트워크 소켓 및 파일 디스크립터와 같은 필요한 리소스를 설정하고,
+ * 들어오는 클라이언트 요청을 처리합니다. 주요 이벤트 루프는 계속해서
+ * 들어오는 연결을 수신하고 요청을 처리하며 적절한 응답을 보냅니다.
+ *
+ * @return 서버가 성공적으로 실행되면 0을 반환하고, 그렇지 않으면 0이 아닌 값을 반환합니다.
+ */
 int httpd_main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
